e26f3f112fa527c64213bac9662109b537b27ffa
Rasul Eletai <121658609+IbnRasAl-Fakih@users.noreply.github.com>
2025-04-30 17:26:25 +0500

    Update SecurityConfig.java

--- backend/src/main/java/com/diploma/config/SecurityConfig.java
+++ backend/src/main/java/com/diploma/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                    "swagger-ui.html",
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
                     "/swagger-resources/**",
